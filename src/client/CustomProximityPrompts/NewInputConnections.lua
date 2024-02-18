3a64128f292603b0161974be42bebb0714ca6bd1
Iconic Gaming <96698128+MiaGobble@users.noreply.github.com>
2024-02-18 12:18:37 -0600

    Added sounds

--- src/client/CustomProximityPrompts/NewInputConnections.lua
+++ src/client/CustomProximityPrompts/NewInputConnections.lua
@@ -1,3 +1,4 @@
+-- Types
 type Properties = {
     prompt: ProximityPrompt,
     ButtonHeldDown: any,
@@ -7,14 +8,61 @@ type Properties = {
     InputFrameScaleFactor: any,
 }
 
+-- Constants
+local APPEAR_SOUND = "rbxassetid://10066931761"
+local CLICK_SOUND = "rbxassetid://10128760939"
+local HOLD_SOUND = "rbxassetid://421058925"
+local TRIGGER_SOUND = "rbxassetid://10128766965"
+
+-- Services
 local RunService = game:GetService("RunService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+-- Imports
+local Fusion = require(ReplicatedStorage.Packages.Fusion)
+local New = Fusion.New
+local Hydrate = Fusion.Hydrate
+
+local function PlaySound(SoundId, Properties : {[string]: any}?)
+	local Sound = script:FindFirstChild(SoundId)
+
+	if not Sound then
+		Sound = New "Sound" {
+			Name = SoundId,
+			SoundId = SoundId,
+			Volume = 0.35,
+			Parent = script,
+		}
+
+		if Properties then
+			Hydrate(Sound)(Properties)
+		end
+	end
+
+	Sound:Play()
+end
+
+local function StopSound(SoundId)
+	local Sound = script:FindFirstChild(SoundId)
+
+	if Sound then
+		Sound:Stop()
+	end
+end
 
 return function(Properties : Properties) : {RBXScriptConnection}
     local InputConnections = {}
+	local HoldStart = os.clock()
+
+	PlaySound(APPEAR_SOUND)
 
     InputConnections[#InputConnections + 1] = RunService.RenderStepped:Connect(function(deltaTime)
+		local TimeHeldDown = os.clock() - HoldStart
+
 		if Properties.ButtonHeldDown:get() then
 			Properties.CurrentBarSize:set(math.min(Properties.CurrentBarSize:get() + deltaTime / Properties.prompt.HoldDuration, 1))
+
+			script:FindFirstChild(HOLD_SOUND).PlaybackSpeed = 0.5 + TimeHeldDown * 1
 		else
 			Properties.CurrentBarSize:set(0)
 		end
@@ -22,21 +70,36 @@ return function(Properties : Properties) : {RBXScriptConnection}
 
 	if Properties.prompt.HoldDuration > 0 then
 		InputConnections[#InputConnections + 1] = Properties.prompt.PromptButtonHoldBegan:Connect(function()
+			HoldStart = os.clock()
+			PlaySound(HOLD_SOUND, {
+				Looped = true,
+				Volume = 0.2,
+			})
+			PlaySound(CLICK_SOUND)
 			Properties.ButtonHeldDown:set(true)
 			Properties.CurrentFrameScaleFactor:set(Properties.InputFrameScaleFactor)
 		end)
 
 		InputConnections[#InputConnections + 1] = Properties.prompt.PromptButtonHoldEnded:Connect(function()
+			StopSound(HOLD_SOUND)
 			Properties.ButtonHeldDown:set(false)
 			Properties.CurrentFrameScaleFactor:set(1)
 		end)
 	end
 
 	InputConnections[#InputConnections + 1] = Properties.prompt.Triggered:Connect(function()
+		print("trigger")
+		if Properties.prompt.HoldDuration > 0 then
+			PlaySound(TRIGGER_SOUND)
+		else
+			PlaySound(CLICK_SOUND)
+		end
+
 		Properties.PromptTransparency:set(1)
 	end)
 
 	InputConnections[#InputConnections + 1] = Properties.prompt.TriggerEnded:Connect(function()
+		print("end")
 		Properties.PromptTransparency:set(1)
 	end)
 
