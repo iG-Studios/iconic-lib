630e34d90533111f97b4a24885eead1822638dd9
Iconic Gaming <96698128+MiaGobble@users.noreply.github.com>
2024-02-18 12:18:23 -0600

    Added rotation and gradient animations

--- src/client/CustomProximityPrompts/BuildFrames.lua
+++ src/client/CustomProximityPrompts/BuildFrames.lua
@@ -13,7 +13,17 @@ return function(PromptUI, ButtonHeldDown, PromptTransparency, AspectRatioValue, 
 	local Frame = New "CanvasGroup" {
 		Size = Spring(Computed(function()
 			return ButtonHeldDown:get() and UDim2.fromScale(0.5, 1) or UDim2.fromScale(1, 1)
-		end), 20, 0.5),
+		end), 20, 0.4),
+
+		Rotation = Spring(Computed(function()
+			if ButtonHeldDown:get() then
+				return -5
+			elseif PromptTransparency:get() < 1 then
+				return 0
+			else
+				return 5
+			end
+		end), 30, 0.1),
 		
 		BackgroundTransparency = 0.5,
 		GroupTransparency = Spring(PromptTransparency, 30, 1),
@@ -28,7 +38,7 @@ return function(PromptUI, ButtonHeldDown, PromptTransparency, AspectRatioValue, 
 			New "UIAspectRatioConstraint" {
 				AspectRatio = Spring(Computed(function()
 					return ButtonHeldDown:get() and 1 or AspectRatioValue:get()
-				end), 20, 0.5),
+				end), 20, 0.4),
 			},
 
 			New "Frame" {
@@ -41,6 +51,21 @@ return function(PromptUI, ButtonHeldDown, PromptTransparency, AspectRatioValue, 
 				Size = Computed(function()
 					return UDim2.fromScale(CurrentBarSize:get(), 0.1)
 				end),
+			},
+
+			New "UIGradient" {
+				Transparency = NumberSequence.new{
+					NumberSequenceKeypoint.new(0, 0),
+					--NumberSequenceKeypoint.new(0.2, 0),
+					NumberSequenceKeypoint.new(0.5, 0.5),
+					--NumberSequenceKeypoint.new(0.8, 0),
+					NumberSequenceKeypoint.new(1, 0),
+				},
+
+				Rotation = 45,
+				Offset = Spring(Computed(function()
+					return PromptTransparency:get() < 1 and Vector2.new(1, 0) or Vector2.new(-1, 0)
+				end), 8, 1),
 			}
 		},
 	}
